a5cd23e52e2aa574810656e3caaef86f43998aee
Bartosz T <bartosz@telenczuk.pl>
2016-05-31 14:54:29 +0200

    add arguments to certificate generation script

--- certificates/generate_certificates.py
+++ certificates/generate_certificates.py
@@ -9,9 +9,16 @@ import subprocess
 import shutil
 import os
 import pandas as pd
+import argparse
 
-participants = pd.DataFrame.from_csv('participants.csv', index_col=None)
-with file('certificate.tex') as fid:
+parser = argparse.ArgumentParser()
+parser.add_argument('participants', help='CSV file with participants')
+parser.add_argument('--template', '-t', default='certificate.tex', help='LaTeX certificate template')
+
+args = parser.parse_args()
+
+participants = pd.DataFrame.from_csv(args.participants, index_col=None)
+with file(args.template) as fid:
     t = jinja2.Template(fid.read().decode('utf-8'))
 
 tmpdir = tempfile.mkdtemp()
@@ -35,4 +42,6 @@ for pid in range(len(participants)):
     import glob
 
     all_pdfs = glob.glob(os.path.join(tmpdir, '*.pdf'))
-subprocess.call(['pdfjoin', '-o', 'certificates.pdf'] + all_pdfs)
+
+pdf_filename = os.path.splitext(args.template)[0] + '.pdf'
+subprocess.call(['pdfjoin', '-o', pdf_filename] + all_pdfs)
